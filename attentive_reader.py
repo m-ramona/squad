49e2934907c96ff560796738beedbd19aae80b05
Mathieu Ramona <mathieu.ramona.pro@gmail.com>
2020-09-06 20:58:04 +0200

    Dropout on word embeddings output

--- attentive_reader.py
+++ attentive_reader.py
@@ -116,6 +116,9 @@ class AttentiveReaderModel(nn.Module):
         c_emb = self.embed(cw_idxs)         # (batch_size, c_len, input_size)
         q_emb = self.embed(qw_idxs)         # (batch_size, q_len, input_size)
 
+        c_emb = F.dropout(c_emb, self.drop_prob, self.training)
+        q_emb = F.dropout(q_emb, self.drop_prob, self.training)
+
         p_i = self.passage_rnn(c_emb, c_len)    # (batch_size, c_len, h)
         q = self.query_rnn(q_emb, q_len)        # (batch_size, 1, h)
         q_t = q.permute(0, 2, 1)                # (batch_size, h, 1)
